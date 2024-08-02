5abef8d2cbf4e2c6315006235c7c426f38d1766f
Jonas Spinner <jonas.spinner@student.kit.edu>
2024-08-02 11:57:47 +0200

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,16 @@ use std::io;
 use std::process;
 
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    if pattern.chars().count() == 1 {
-        return input_line.contains(pattern);
-    } else {
-        panic!("Unhandled pattern: {}", pattern)
+    if pattern == "\\d" {
+        return input_line.contains(|c| matches!(c, '0'..='9'))
+    } else if pattern.len() == 1 {
+        if let Some(c) = pattern.chars().next() {
+            if matches!(c, 'a'..='z' | 'A'..='Z') {
+                return input_line.contains(c);
+            }
+        }
     }
+    panic!("Unhandled pattern: {}", pattern)
 }
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
